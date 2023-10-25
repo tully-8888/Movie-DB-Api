83bfba1c6bd2e33f146ba10368ffcfe079e19393
Tulitu Vlad <tulituvlad@gmail.com>
2023-10-25 19:14:14 +0300

    Implement models

--- src/main/kotlin/model/Movie.kt
+++ src/main/kotlin/model/Movie.kt
@@ -1,5 +1,24 @@
 package model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+
+@Entity
 data class Movie(
-    var placeholder:String=""
-)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val title: String = "",
+    val description: String = "",
+    val genre: String = "",
+    val tags: String = "",
+    val length: Int = 0,
+    val dateReleased: String = "",
+    val dateAvailableUntil: String = "",
+    val metaTitle: String? = null,
+    val metaDescription: String? = null
+) {
+    constructor() : this(0, "", "", "", "", 0, "", "", null, null)
+}
--- src/main/kotlin/model/User.kt
+++ src/main/kotlin/model/User.kt
@@ -1,5 +1,20 @@
 package model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
 data class User(
-    var placeholder:String=""
-)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val email: String = "",
+    val phoneNumber: String? = null,
+    val favoriteMovies: String? = null,
+    val favoriteCategories: String? = null,
+    val websiteTheme: String? = null
+) {
+    constructor() : this(0, "", null, null, null, null)
+}
+
