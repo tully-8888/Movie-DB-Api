0efa061fcd0388a2c925f3b14fa7a7880c3d9202
Tulitu Vlad <tulituvlad@gmail.com>
2023-10-25 19:18:49 +0300

    Implement controllers

--- src/main/kotlin/controller/MovieController.kt
+++ src/main/kotlin/controller/MovieController.kt
@@ -1,4 +1,23 @@
 package controller
 
-class MovieController {
+import model.Movie
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import repository.MovieRepository
+
+@RestController
+@RequestMapping("/api")
+class MovieController(@Autowired private val movieRepository: MovieRepository) {
+
+    @GetMapping("/movies")
+    fun getAllMovies(): MutableList<Movie> = movieRepository.findAll()
+
+    @GetMapping("/movies/{genre}")
+    fun getMoviesByGenre(@PathVariable genre: String) = movieRepository.findByGenre(genre)
+
+    @GetMapping("/movies/tag/{tag}")
+    fun getMoviesByTag(@PathVariable tag: String) = movieRepository.findByTags(tag)
 }
--- src/main/kotlin/controller/UserController.kt
+++ src/main/kotlin/controller/UserController.kt
@@ -1,4 +1,21 @@
 package controller
 
-class UserController {
+import model.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import repository.UserRepository
+import java.util.Optional
+
+@RestController
+@RequestMapping("/api")
+class UserController(@Autowired private val userRepository: UserRepository) {
+
+    @GetMapping("/users")
+    fun getAllUsers(): MutableList<User> = userRepository.findAll()
+
+    @GetMapping("/user/{id}")
+    fun getUserById(@PathVariable id: Long): Optional<User> = userRepository.findById(id)
 }
