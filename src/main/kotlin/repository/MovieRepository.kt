fb70b9b2ff8d16d74aea04bc24d4ba1e022de1ef
Tulitu Vlad <tulituvlad@gmail.com>
2023-10-25 19:16:09 +0300

    Implement repositories

--- src/main/kotlin/repository/MovieRepository.kt
+++ src/main/kotlin/repository/MovieRepository.kt
@@ -1,4 +1,11 @@
 package repository
 
-class MovieRepository {
+import model.Movie
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MovieRepository : JpaRepository<Movie, Long> {
+    fun findByGenre(genre: String): List<Movie>
+    fun findByTags(tag: String): List<Movie>
 }
--- src/main/kotlin/repository/UserRepository.kt
+++ src/main/kotlin/repository/UserRepository.kt
@@ -1,4 +1,8 @@
 package repository
 
-class UserRepository {
-}
+import model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long>
