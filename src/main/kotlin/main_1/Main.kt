5c8801c5842a307d4c6c134157788369b9d68e05
Tulitu Vlad <tulituvlad@gmail.com>
2023-10-26 11:24:50 +0300

    Add documentation

--- src/main/kotlin/main_1/Main.kt
+++ src/main/kotlin/main_1/Main.kt
@@ -12,15 +12,47 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 
+/**
+ * Entry point for the Spring Boot application.
+ * Scans the specified packages for entity classes.
+ */
 @SpringBootApplication
 @EntityScan(basePackages = ["main_1.model"])
 open class MyApplication
 
-    fun main(args: Array<String>) {
-        val context: ApplicationContext = runApplication<MyApplication>(*args)
+/**
+ * Main function to launch the Spring Boot application.
+ * @param args Command line arguments.
+ */
+fun main(args: Array<String>) {
+    val context: ApplicationContext = runApplication<MyApplication>(*args)
 
-        val movieService = context.getBean(MovieService::class.java)
-        val userService = context.getBean(UserService::class.java)
+    /**
+     * Retrieve MovieService bean.
+     */
+    val movieService = context.getBean(MovieService::class.java)
 
-    }
+    /**
+     * Retrieve UserService bean.
+     */
+    val userService = context.getBean(UserService::class.java)
 
+}
+
+/*
+Class: MyApplication
+
+Description:
+The MyApplication class serves as the entry point for the Spring Boot application. It includes the @SpringBootApplication and @EntityScan annotations to bootstrap the application and scan the specified package for entity classes. The main function initializes the Spring application context and retrieves the necessary beans for MovieService and UserService.
+
+Annotations:
+- @SpringBootApplication: Specifies that the class serves as the entry point for a Spring Boot application.
+- @EntityScan: Specifies the base packages to scan for entity classes.
+
+Functions:
+
+- main
+    - Parameters:
+        - args (Array<String>): Command line arguments.
+    - Description: Initializes the Spring application context and retrieves the necessary beans for MovieService and UserService.
+*/
