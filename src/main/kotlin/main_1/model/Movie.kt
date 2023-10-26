3d3a990dcf2b1c3b2d7040e1b9710f0e8e4c72a3
Tulitu Vlad <tulituvlad@gmail.com>
2023-10-26 11:15:55 +0300

    Updated rest apis

--- src/main/kotlin/main_1/model/Movie.kt
+++ src/main/kotlin/main_1/model/Movie.kt
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
-
 @Entity
 data class Movie(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
